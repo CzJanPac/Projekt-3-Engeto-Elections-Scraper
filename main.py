443b57c47ee89d86dacfd6c35a7be3e2620d5830
CzJanPac <czjanpac@gmail.com>
2025-08-05 06:56:51 +0200

    zalozeni projektu

---
+++ main.py
@@ -0,0 +1,24 @@
+"""
+main.py: třetí projekt do Engeto Online Python Akademie
+
+author: Jan Páč
+email: czjanpac@gmail.com
+"""
+
+from bs4 import BeautifulSoup as bs
+import requests
+
+def nacti_zadany_odkaz(odkaz):
+    pass
+
+def ziskej_parsovanou_odpoved(odkaz):
+    pass
+
+def najdi_volebni_data(odkaz):
+    pass
+
+def zapis_data_do_csv(soubor):
+    pass
+
+def main():
+    pass
